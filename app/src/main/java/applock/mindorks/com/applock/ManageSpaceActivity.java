f91474f2c3dc216ef153fa7aecf71be13e0edd75
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2015-05-02 21:46:49 +0530

    user cannot delete data done

---
+++ app/src/main/java/applock/mindorks/com/applock/ManageSpaceActivity.java
@@ -0,0 +1,16 @@
+package applock.mindorks.com.applock;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amitshekhar on 02/05/15.
+ */
+public class ManageSpaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
